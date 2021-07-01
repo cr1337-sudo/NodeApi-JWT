b909e60f0fc16be3e89d3c762d271fe661b22be7
Cristian <cristiancuello10@gmail.com>
2021-07-01 20:40:11 -0300

    api

---
+++ README.md
@@ -0,0 +1,8 @@
+API BÁSICA JWT
+
+Control de login y registro básico.
+
+Cosas a mejorar:
+-Agregar roles
+-Restringir URLS
+-Crear variables de entorno
--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,62 @@
 const { Router } = require("express");
 const routerAuth = Router()
 const User = require("../models/User")
+const { registerValidation, loginValidation } = require("../validation")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
-routerAuth.post("/register", async (req,res)=>{
 
+routerAuth.post("/register", async (req, res) => {
+	const { error } = registerValidation(req.body)
+	if (error) {
+		res.status(400).send(error.details[0].message)
+	}
+
+	//Checking if the user is already in the databse
+	const emailExist = await User.findOne({ email: req.body.email })
+	if (emailExist) return res.status(400).send("Email already exists")
+
+	//Hach passwords
+	const salt = await bcrypt.genSalt(10)
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	//Create a nwe user
 	const user = new User({
 		name: req.body.name,
-		email : req.body.email,
-		password : req.body.password
+		email: req.body.email,
+		password: hashedPassword
 	})
-	
-	try{
+
+	try {
 		const savedUser = await user.save()
-		res.send(savedUser)
-	}catch(eror){
+		res.send({ user: savedUser.name })
+	} catch (eror) {
 		res.status(400).send(err)
 	}
 })
 
 
+//Login
+routerAuth.post("/login", async (req, res) => {
+	const { error } = loginValidation(req.body)
+	if (error) {
+		res.status(400).send(error.details[0].message)
+	}
+	//Checking if the user is already in the databse
+	const user = await User.findOne({ email: req.body.email })
+	if (!user) return res.status(400).send("Email doesnt exists")
+
+	//Check if password is correct
+	const validPass = await bcrypt.compare(req.body.password, user.password)
+	if (!validPass) return res.status(400).send("Invalid password")
+
+	//Create and assing a token
+	const secretPass = "kjkszpj"
+	const token = jwt.sign({_id: user._id}, secretPass);
+
+	res.header("auth-token", token).send(token);
+
+
+})
 
 module.exports = routerAuth
